d99178e14ce127cbf87fef37e38ac7ab8f86f111
Kostiantyn Vorobiov <kostiantyn_vorobiov@epam.com>
2018-11-15 12:25:34 +0200

    back to wc

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -3,14 +3,7 @@
 def call() {
     
 
-env.sonarHome= tool name: 'SonarQubeScanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
-
-    withSonarQubeEnv('sonarqube') { // from SonarQube servers > name
-    sh "${sonarHome}/bin/sonar-runner -Dsonar.host.url=${SONAR_HOST_URL}  -Dsonar.login=${SONAR_AUTH_TOKEN} -Dsonar.projectName=test -Dsonar.projectVersion=1 -Dsonar.projectKey=my -Dsonar.sources=."
-}
-
-
-/*    withSonarQubeEnv('sonarqube') {
+    withSonarQubeEnv('sonarqube') {
         sh 'mvn sonar:sonar'
-    	}*/
+    }	
 }
