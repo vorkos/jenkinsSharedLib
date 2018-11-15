a965b80a127f54f9c01e9073464b435237507083
Kostiantyn Vorobiov <kostiantyn_vorobiov@epam.com>
2018-11-15 11:49:03 +0200

    back to working ver

--- src/com/buildlib/sonar.groovy
+++ src/com/buildlib/sonar.groovy
@@ -1,7 +1,8 @@
 package com.buildlib
 @Grab(group = 'org.apache.commons', module = 'commons-lang3', version = '3.6')
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
 import org.apache.commons.lang3.StringUtils
-
+import static groovyx.net.http.ContentType.TEXT
 /**
 *
 *
@@ -9,5 +10,10 @@ import org.apache.commons.lang3.StringUtils
 
 class SonarQube implements Serializable {
     def script
+    def steps
     protected Map config
+    String taskURL = "http://<SONAR_URL>/api/ce/task?id=TASK_ID"
+    String projectStatusURL = "http://<SONAR_URL>/api/qualitygates/project_status?analysisId="
+    SonarQube(steps) {this.steps = steps}
+    /* -Dsonar.login= -Dsonar.projectKey=myproject -Dsonar.projectName= */
 }
